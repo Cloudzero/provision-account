668e4db4b75fc181c38cf24a15d6271a8474f4d5
Adam Tankanow <adam.tankanow@gmail.com>
2019-07-10 17:20:35 -0400

    CP-901: Discovery Lambda with Tests

--- services/discovery/src/app.py
+++ services/discovery/src/app.py
@@ -3,15 +3,20 @@
 # Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 
 from pprint import pformat
-# import boto3
+import boto3
 import cfnresponse
-from toolz.curried import assoc_in, pipe
+from toolz.curried import assoc_in, get_in, keyfilter, pipe
 from voluptuous import Any, Schema, ALLOW_EXTRA, REMOVE_EXTRA
 
 
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+
+ct = boto3.client('cloudtrail')
+cur = boto3.client('cur')
+s3 = boto3.client('s3')
 
 
 #####################
@@ -21,17 +26,30 @@ logger.setLevel(logging.INFO)
 #####################
 INPUT_SCHEMA = Schema({
     'event': {
-        'ResourceProperties': dict,
+        'ResourceProperties': {
+            'accountId': str
+        },
         'PhysicalResourceId': Any(None, str),
     }
 }, required=True, extra=REMOVE_EXTRA)
 
 OUTPUT_SCHEMA = Schema({
     'output': {
-        'isAudit': bool,
-        'isConnectable': bool,
-        'isCloudTrailOwner': bool,
-        'isMasterPayer': bool,
+        'audit': {
+            'accountId': str,
+            'cloudTrailBucketName': str
+        },
+        'connected': {
+            'accountId': str,
+        },
+        'cloudTrail': {
+            'accountId': str,
+            'snsTopicName': str,
+        },
+        'masterPayer': {
+            'accountId': str,
+            'billingBucketName': str,
+        },
     },
 }, required=True, extra=ALLOW_EXTRA)
 
@@ -42,7 +60,41 @@ OUTPUT_SCHEMA = Schema({
 #
 #####################
 def coeffects(world):
-    return pipe(world)
+    return pipe(world,
+                coeffects_cloudtrail,
+                coeffects_s3,
+                coeffects_cur)
+
+
+def coeffect(name):
+    def d(f):
+        def w(world):
+            data = {}
+            try:
+                data = f(world)
+            except Exception:
+                logger.warning(f'Failed to get {name} information.')
+            return assoc_in(world, ['coeffects', name], data)
+        return w
+    return d
+
+
+@coeffect('cloudtrail')
+def coeffects_cloudtrail(world):
+    response = ct.describe_trails()
+    return keyfilter(lambda x: x in {'trailList'}, response)
+
+
+@coeffect('s3')
+def coeffects_s3(world):
+    response = s3.list_buckets()
+    return keyfilter(lambda x: x in {'Buckets'}, response)
+
+
+@coeffect('cur')
+def coeffects_cur(world):
+    response = cur.describe_report_definitions()
+    return keyfilter(lambda x: x in {'ReportDefinitions'}, response)
 
 
 #####################
@@ -50,14 +102,51 @@ def coeffects(world):
 # Business Logic
 #
 #####################
-def discover_account_types(world):
+def audit(world):
+    account_id = get_in(['event', 'ResourceProperties', 'accountId'], world)
+    trail_bucket = get_in(['coeffects', 'cloudtrail', 'trailList', 0, 'S3BucketName'], world)
+    local_buckets = {x['Name'] for x in get_in(['coeffects', 's3', 'Buckets'], world, [])}
     output = {
-        'isAudit': True,
-        'isConnectable': True,
-        'isCloudTrailOwner': True,
-        'isMasterPayer': True,
+        'accountId': account_id if trail_bucket in local_buckets else None,
+        'cloudTrailBucketName': trail_bucket,
     }
-    return assoc_in(world, ['output'], output)
+    return assoc_in(world, ['output', 'audit'], output)
+
+
+def connected(world):
+    output = {
+        'accountId': get_in(['event', 'ResourceProperties', 'accountId'], world),
+    }
+    return assoc_in(world, ['output', 'connected'], output)
+
+
+def cloudtrail(world):
+    trail_topic = get_in(['coeffects', 'cloudtrail', 'trailList', 0, 'SnsTopicName'], world)
+    account_id = trail_topic.split(':')[4]
+    output = {
+        'accountId': account_id,
+        'snsTopicName': trail_topic,
+    }
+    return assoc_in(world, ['output', 'cloudTrail'], output)
+
+
+def master_payer(world):
+    account_id = get_in(['event', 'ResourceProperties', 'accountId'], world)
+    payer_bucket = get_in(['coeffects', 'cur', 'ReportDefinitions', 0, 'S3Bucket'], world)
+    local_buckets = {x['Name'] for x in get_in(['coeffects', 's3', 'Buckets'], world, [])}
+    output = {
+        'accountId': account_id if payer_bucket in local_buckets else None,
+        'billingBucketName': payer_bucket,
+    }
+    return assoc_in(world, ['output', 'masterPayer'], output)
+
+
+def discover_account_types(world):
+    return pipe(world,
+                audit,
+                connected,
+                cloudtrail,
+                master_payer)
 
 
 #####################
--- services/discovery/tests/unit/test_handler.py
+++ services/discovery/tests/unit/test_handler.py
@@ -2,26 +2,106 @@
 # Copyright (c) 2016-present, CloudZero, Inc. All rights reserved.
 # Licensed under the BSD-style license. See LICENSE file in the project root for full license information.
 
+import os
+
+import attrdict
 import cfnresponse
 import pytest
 
 import src.app as app
 
 
+LOCAL_ACCOUNT_ID = '123456789012'
+REMOTE_ACCOUNT_ID = '999999999999'
+
+LOCAL_BUCKET_NAME = 'local-bucket'
+REMOTE_BUCKET_NAME = 'remote-bucket'
+
+LOCAL_TRAIL_ARN = f'arn:aws:cloudtrail:us-east-1:{LOCAL_ACCOUNT_ID}:trail/local-trail'
+REMOTE_TRAIL_ARN = f'arn:aws:cloudtrail:us-east-1:{REMOTE_ACCOUNT_ID}:trail/remote-trail'
+
+LOCAL_TOPIC_ARN = f'arn:aws:sns:us-east-1:{LOCAL_ACCOUNT_ID}:local-cloudtrail-topic'
+REMOTE_TOPIC_ARN = f'arn:aws:sns:us-east-1:{REMOTE_ACCOUNT_ID}:remote-cloudtrail-topic'
+
+
 @pytest.fixture()
 def cfn_event():
     return {
-        'ResourceProperties': {},
+        'ResourceProperties': {
+            'accountId': LOCAL_ACCOUNT_ID,
+        },
         'PhysicalResourceId': None,
     }
 
 
+@pytest.fixture()
+def describe_trails_response():
+    return {
+        'trailList': [
+            {
+                'HasCustomEventSelectors': True,
+                'HomeRegion': 'us-east-1',
+                'IncludeGlobalServiceEvents': True,
+                'IsMultiRegionTrail': True,
+                'IsOrganizationTrail': False,
+                'LogFileValidationEnabled': True,
+                'Name': 'my-local-trail',
+                'S3BucketName': LOCAL_BUCKET_NAME,
+                'SnsTopicARN': LOCAL_TOPIC_ARN,
+                'SnsTopicName': LOCAL_TOPIC_ARN,
+                'TrailARN': LOCAL_TRAIL_ARN,
+            },
+        ],
+    }
+
+
+@pytest.fixture()
+def list_buckets_response():
+    return {
+        'Buckets': [
+            {
+                'Name': LOCAL_BUCKET_NAME,
+            }
+        ]
+    }
+
+
+@pytest.fixture()
+def describe_report_definitions_response():
+    return {
+        'ReportDefinitions': [
+            {
+                'S3Bucket': LOCAL_BUCKET_NAME,
+                'S3Prefix': 'path',
+                'ReportName': 'billing_report',
+            }
+        ]
+    }
+
+
+@pytest.fixture(scope='function')
+def context(mocker):
+    context = attrdict.AttrMap()
+    orig_env = os.environ.copy()
+    context.os = {'environ': os.environ}
+    context.prefix = app.__name__
+    context.mock_cfnresponse_send = mocker.patch(f'{context.prefix}.cfnresponse.send', autospec=True)
+    context.mock_ct = mocker.patch(f'{context.prefix}.ct', autospec=True)
+    context.mock_cur = mocker.patch(f'{context.prefix}.cur', autospec=True)
+    context.mock_s3 = mocker.patch(f'{context.prefix}.s3', autospec=True)
+    yield context
+    os.environ = orig_env
+    mocker.stopall()
+
+
 @pytest.mark.unit
-def test_handler(cfn_event, mocker):
-    cfnresponse = mocker.patch(f'{app.__name__}.cfnresponse', autospec=True)
+def test_handler(context, cfn_event, describe_trails_response, list_buckets_response, describe_report_definitions_response):
+    context.mock_ct.describe_trails.return_value = describe_trails_response
+    context.mock_cur.describe_report_definitions.return_value = describe_report_definitions_response
+    context.mock_s3.list_buckets.return_value = list_buckets_response
     ret = app.handler(cfn_event, None)
     assert ret is None
-    assert cfnresponse.send.call_count == 1
-    ((_, _, status, output, _), kwargs) = cfnresponse.send.call_args
+    assert context.mock_cfnresponse_send.call_count == 1
+    ((_, _, status, output, _), kwargs) = context.mock_cfnresponse_send.call_args
     assert status == cfnresponse.SUCCESS
     app.OUTPUT_SCHEMA({'output': output})
