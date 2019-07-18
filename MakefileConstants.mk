99f387990ea99dfef72583605806ea542e6ca999
Adam Tankanow <adam.tankanow@gmail.com>
2019-07-18 14:31:25 -0400

    CP-928: Add Audit Account CFN Template (#10)
    * CP-928: Add audit.yaml
    
    * CP-928: Add audit.yaml to CFN_TEMPLATES
    
    * CP-928: Dynamically find templates and sam apps
    
    * CP-928: Fix copy/pasta

--- MakefileConstants.mk
+++ MakefileConstants.mk
@@ -5,29 +5,22 @@
 # Main parameters
 FEATURE_NAME ?= provision-account
 TEAM_NAME ?= cloudzero
-BUCKET = cz-$(FEATURE_NAME)
-SEMVER_MAJ_MIN = 1.0
-
-# Source Files
-CFN_TEMPLATES = services/connected_account.yaml services/account_type/cloudtrail_owner.yaml services/account_type/resource_owner.yaml services/account_type/master_payer.yaml
-SAM_APPS = services/discovery
+BUCKET := cz-$(FEATURE_NAME)
+SEMVER_MAJ_MIN := 1.0
 
 # Util constants
-ERROR_COLOR = \033[1;31m
-INFO_COLOR = \033[1;32m
-WARN_COLOR = \033[1;33m
-NO_COLOR = \033[0m
+ERROR_COLOR := \033[1;31m
+INFO_COLOR := \033[1;32m
+WARN_COLOR := \033[1;33m
+NO_COLOR := \033[0m
 
 # Prerequisite verification
-REQUIREMENTS_FILES = $(shell find . -name "requirements*.txt")
-PYTHON_DEPENDENCY_FILE = .cz_py_dependencies_installed
+REQUIREMENTS_FILES := $(shell find . -name "requirements*.txt")
+PYTHON_DEPENDENCY_FILE := .cz_py_dependencies_installed
 
 # Testing
-CFN_LINT_OUTPUT = cfn-lint.output
-
-# Git
-# GIT_REV := $(shell git rev-parse HEAD)
+CFN_LINT_OUTPUT := cfn-lint.output
 
 # CFN Constants
-TEMPLATE_FILE = template.yaml
-PACKAGED_TEMPLATE_FILE = packaged-template.yaml
+TEMPLATE_FILE := template.yaml
+PACKAGED_TEMPLATE_FILE := packaged-template.yaml
