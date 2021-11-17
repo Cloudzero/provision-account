349bf3f26e063e563d6c315d817b216ba9d72230
Kevin Mueller <kevin.mueller@cloudzero.com>
2021-11-17 16:20:32 -0500

    initial setup.

---
+++ helm-charts/README.md
@@ -0,0 +1,16 @@
+[![CircleCI](https://circleci.com/gh/aws/eks-charts.svg?style=svg)](https://circleci.com/gh/aws/eks-charts)
+
+## EKS Charts
+
+Add the EKS repository to Helm:
+
+```sh
+helm repo add cloudzero https://cloudzero.github.io/helm-charts
+```
+
+### AWS CloudWatch Metrics
+* [cloudzero-cloudwatch-metrics](stable/cloudzero-cloudwatch-metrics): A helm chart for CloudWatch Agent to Collect Cluster Metrics
+
+## License
+
+This project is licensed under the Apache-2.0 License.
---
+++ helm-charts/test/lib/kind.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Create Kubernetes cluster with Kind
+
+set -o errexit
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+TOOLS_DIR="$REPO_ROOT/build/tools"
+export PATH="$TOOLS_DIR:$PATH"
+HELM_MODE="${HELM_MODE:-v2}"
+
+kind create cluster --wait 5m
+
+function installHelm() {
+
+  if [[ "${1}" == "v2" ]]; then
+    kubectl --namespace kube-system create sa tiller
+
+    kubectl create clusterrolebinding tiller-cluster-rule \
+      --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+
+    cp -f $TOOLS_DIR/helmv2 $TOOLS_DIR/helm
+
+    helm init --stable-repo-url https://charts.helm.sh/stable --service-account tiller --upgrade --wait
+  else
+    cp -f $TOOLS_DIR/helmv3 $TOOLS_DIR/helm
+  fi
+}
+
+export KUBECONFIG="$(kind get kubeconfig > $REPO_ROOT/build/kind-kubeconfig)"
+
+installHelm $HELM_MODE
+
+kubectl get pods --all-namespaces
