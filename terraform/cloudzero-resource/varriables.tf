7febd2bfadee554f5cf452c67990ba6a888a6662
khill2018 <khill@wellesley.edu>
2026-05-26 15:19:46 -0400

    match existing reactor principal one for one

--- terraform/cloudzero-resource/varriables.tf
+++ terraform/cloudzero-resource/varriables.tf
@@ -2,3 +2,9 @@ variable "cloudzero_external_id" {
   type = string
   description = "The CloudZero provided External ID for your cross account access role (Your ID can be found on the CloudZero manual account connection page)"
 }
+
+variable "connectors_account_id" {
+  type        = string
+  default     = "931830253929"
+  description = "Additional CloudZero AWS account ID that will assume the cross-account role. Override only if directed by CloudZero support."
+}
